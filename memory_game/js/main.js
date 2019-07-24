01c197004a37bd11e722dd96f8b23943870eabbf
Jennifer Godley <jenny@Jennifers-MacBook-Air.local>
2019-07-24 13:02:37 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+//checking console
+console.log("Up and running!");
+//creating cards
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+//display value of two cards when flipped
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
