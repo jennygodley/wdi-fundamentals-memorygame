39f11547eac8b6286c161456f715fb1343b51161
Jennifer Godley <jenny@Jennifers-MacBook-Air.local>
2019-07-25 13:06:35 -0400

    created game logic

--- memory_game/js/main2.js
+++ memory_game/js/main2.js
@@ -1,7 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+var cardsInPlay = [];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+// if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+// }
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
