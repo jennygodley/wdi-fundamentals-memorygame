8aab924b3ee864b6981bd4e55e5339b6e32b6b8a
Jennifer Godley <jenny@Jennifers-MacBook-Air.local>
2019-07-25 09:24:50 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,41 @@
-var cards = ["queen", "queen", "king", "king"]; // card array
+var cards = [  // card array
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+];
 var cardsInPlay = []; // empty array
 function checkForMatch(){
 if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
-}
+};
+
 function flipCard(cardId) {
-console.log("User flipped " + cards[cardId]) // console showing card in play
+console.log("User flipped " + cards[cardId].rank); // console showing card in play
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 checkForMatch();
 };
 
 flipCard(0);
 flipCard(2);
-
-// cardsInPlay.push(cards[cardID]); // not sure where this goes
